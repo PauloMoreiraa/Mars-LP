9e92c8b0982949e74f4fc6b97534a71986d9527f
Paulo Moreira <56117238+PauloMoreiraa@users.noreply.github.com>
2024-03-11 12:14:31 -0300

    :globe_with_meridians: Vídeo inserido

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset: true })
+sr.reveal('.card', {duration: 2000, scale: 0.70});
+
+sr.reveal('.topico', {duration: 2000, scale: 0.70});
+
+sr.reveal('.video', {duration: 2000, scale: 0.20});
+
+sr.reveal('.parte-1', {duration: 1000, scale: 0.70});
